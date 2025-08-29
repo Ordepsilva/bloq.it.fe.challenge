f9f0dbb4677b80d7ef028e02e3ff2d8c68605191
Pedro Silva <pedro-manuel16@hotmail.com>
2025-08-29 20:57:22 +0100

    feat: improve layout bg

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import AppNavbar from '@/components/ui/navbar/AppNavbar.vue';
+import AppNavbar from '@/components/navbar/AppNavbar.vue';
 </script>
 
 <template>
-  <div class="min-h-screen flex-col">
+  <div class="min-h-screen flex-col bg-gradient-to-br from-yellow-100 via-blue-50 to-red-100">
     <AppNavbar />
     <main class="flex-1 p-4 px-2">
       <RouterView />
